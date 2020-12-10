9005287738117899ceaf62950670e355c7e292ca
zulqarnain-99 <zulqi99@gmail.com>
2020-12-11 01:57:14 +0500

    bind data from in tbl in dropdown list

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+
+namespace DataBindDropdownList_AspDotNet
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         
+            if(!IsPostBack)
+            {
+                string CS = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(CS))
+                {
+                    SqlCommand cmd = new SqlCommand("Select * from tblCity", con);
+                    con.Open();
+                    DropDownList1.DataSource = cmd.ExecuteReader();
+                    DropDownList1.DataTextField = "CityName";
+                    DropDownList1.DataValueField = "CityId";
+                    DropDownList1.DataBind();
+
+                }
+            }
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             
+        }
+    }
+}
